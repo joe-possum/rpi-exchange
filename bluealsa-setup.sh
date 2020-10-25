5b2daaa05b7ead044fc7e8d3ee6886b0af467eb3
Macbook Pro 2012 <macbookpro2012@Macbooks-MacBook-Pro.local>
2020-10-24 19:58:56 -0500

    1st try

--- bluealsa-setup.sh
+++ bluealsa-setup.sh
@@ -1,37 +1,9 @@
-    8  apt install mplayer
-   38  apt install autoreconf
-   42  apt install autotools-dev
-   43  apt install dh-autoreconf
-   49  apt install libasound2-dev
-   51  apt install libbluetooth-dev
-   53  apt install libdbus-1-dev/stable 1.12.20-0+deb10u1 
-   54  apt install libdbus-1-dev
-   56  apt install libglib2.0-dev
-   58  apt install libsbc-dev
-   61  apt install libaacs-dev
-   81  apt install bluealsa
-  105  history | grep "apt install"
-  108  history | grep "apt install" > bluealsa-setup.sh
-   11  git clone http://github.com/joe-possum/rpi-exchange
-   12  sudo apt install git
-   13  git clone http://github.com/joe-possum/rpi-exchange
-   17  mv rpi-exchange/.git/ .ssh/
-   20  git pull
-   21  git pull authorized_keys
-   22  git log
-   23  git restore
-   24  git update
-   25  git help
-   26  git checkout
-   27  git checkout authorized_keys
-   28  git checkout
-   29  git status
-   31  wget -q https://github.com/nicokaiser/rpi-audio-receiver/archive/master.zip
-  100  git clone https://github.com/Arkq/bluez-alsa.git
-  162  git clone https://github.com/mstorsjo/fdk-aac.git
-  218  git cloneange
-  219  git clone http://github.com/joe-possum/rpi-exchange
-  222  git pull
-  226  git clone http://github.com/joe-possum/rpi-exchange
-  229  history | grep git
-  230  history | grep git >> bluealsa-setup.sh 
+#! /bin/bash
+
+sudo apt install autoconf dh-autoreconf libasound2-dev libbluetooth-dev libdbus-1-dev libglib2.0-dev libsbc-dev git
+mkdir src && cd src
+git clone https://github.com/mstorsjo/fdk-aac.git
+git clone https://github.com/Arkq/bluez-alsa.git
+
+cd ~/src/fdk-aac && autoreconf --install && mkdir build && cd build && ../configure && make && sudo make install
+cd ~/src/bluez-alsa && autoreconf --install && mkdir build && cd build && ../configure --enable-aac --enable-ofono --enable-debug && make && sudo make install
