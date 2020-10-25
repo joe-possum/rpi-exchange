7f8052cfb91bcf6ed3f9fdd621ce00c6e61358aa
Your Name <you@example.com>
2020-10-25 01:28:42 +0100

    as pi

--- bluealsa-setup.sh
+++ bluealsa-setup.sh
@@ -12,3 +12,26 @@
    81  apt install bluealsa
   105  history | grep "apt install"
   108  history | grep "apt install" > bluealsa-setup.sh
+   11  git clone http://github.com/joe-possum/rpi-exchange
+   12  sudo apt install git
+   13  git clone http://github.com/joe-possum/rpi-exchange
+   17  mv rpi-exchange/.git/ .ssh/
+   20  git pull
+   21  git pull authorized_keys
+   22  git log
+   23  git restore
+   24  git update
+   25  git help
+   26  git checkout
+   27  git checkout authorized_keys
+   28  git checkout
+   29  git status
+   31  wget -q https://github.com/nicokaiser/rpi-audio-receiver/archive/master.zip
+  100  git clone https://github.com/Arkq/bluez-alsa.git
+  162  git clone https://github.com/mstorsjo/fdk-aac.git
+  218  git cloneange
+  219  git clone http://github.com/joe-possum/rpi-exchange
+  222  git pull
+  226  git clone http://github.com/joe-possum/rpi-exchange
+  229  history | grep git
+  230  history | grep git >> bluealsa-setup.sh 
