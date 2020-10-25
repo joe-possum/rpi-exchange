58c513193c1fb54867077b13e92406122838dec2
Your Name <you@example.com>
2020-10-25 01:24:16 +0100

    as root

---
+++ bluealsa-setup.sh
@@ -0,0 +1,14 @@
+    8  apt install mplayer
+   38  apt install autoreconf
+   42  apt install autotools-dev
+   43  apt install dh-autoreconf
+   49  apt install libasound2-dev
+   51  apt install libbluetooth-dev
+   53  apt install libdbus-1-dev/stable 1.12.20-0+deb10u1 
+   54  apt install libdbus-1-dev
+   56  apt install libglib2.0-dev
+   58  apt install libsbc-dev
+   61  apt install libaacs-dev
+   81  apt install bluealsa
+  105  history | grep "apt install"
+  108  history | grep "apt install" > bluealsa-setup.sh
